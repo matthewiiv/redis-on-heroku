b3f8715b6581d254e4f1c7a4d2b3c5ce9e27f9ba
Richard Warren <rwarren92@hotmail.com>
2016-07-07 10:15:05 +0100

    heroku

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const port = 4000;
 function startServer() {
   server.listen(process.env.PORT || port);
   console.log('.txt file has loaded and Server is running on: ' + port);
-})};
+};
 
 startServer();
 
